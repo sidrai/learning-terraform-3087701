b68bcf311554d8c4de54e753ffcf679371b19a48
Siddharth Rai <sidrai@github.com>
2024-12-07 12:59:35 +0000

    main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,33 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-linux-debian-11-x86_64-hvm-ebs-nami"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["895370532475"] # Bitnami
+provider "google" {
+  # Configure the Google Cloud provider
+  credentials = file("/home/siddharth_rai/tfgcpassign-2447c81543d0.json")
+  project     = "tfGcpAssign"
+  region      = "us-central1"
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
+data "google_compute_image" "debian" {
+  family  = "debian-11"
+  project = "debian-cloud"
+}
 
-  tags = {
-    Name = "HelloWorld"
+resource "google_compute_instance" "web" {
+  name         = "HelloWorld-instance"
+  machine_type = "f1-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = data.google_compute_image.debian.self_link
+    }
   }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  tags = ["HelloWorld"]
 }
